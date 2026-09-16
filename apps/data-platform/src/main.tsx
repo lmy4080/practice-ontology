49ae145ac624fcd40175dea1f32ea0c8e760ba8b
이민영 <alsdudwlwhs1@naver.com>
2026-09-16 12:44:54 +0900

    feat: add data platform app shell

---
+++ apps/data-platform/src/main.tsx
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import App from "./App";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
